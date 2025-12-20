6897ceabf3221cc5054e0349b521984e104d22f5
Sajjad Afzaal <sajjadafzaal13416@gmail.com>
2025-12-20 10:48:13 +0500

    add user using sign up form

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import {useState} from 'react'
 import { Link } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  function handleChange(e){
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    });
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log("Response:", data);
+  }
+  console.log(formData);
+
   return (
     <div className='max-w-lg mx-auto p-3'>
       <h1 className='text-3xl font-semibold my-7 text-center'>Sign Up</h1>
-      <form className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg bg-white' id='username'/>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg bg-white' id='email'/>
-        <input type="password" placeholder='password' className='border p-3 rounded-lg bg-white' id='password'/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80'>Sign Up</button>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg bg-white' id='username'  onChange={handleChange}/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg bg-white' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='password' className='border p-3 rounded-lg bg-white' id='password' onChange={handleChange}/>
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80' type='submit'>Sign Up</button>
       </form>
 
       <div className='flex justify-center items-center gap-2 mt-4'>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
-    historyApiFallback: true
-  }
-})
+    historyApiFallback: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, "/api"),
+      },
+    },
+  },
+});
